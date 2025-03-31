0ec3905112261b2a40c84775aae56523d6f5e71d
Djapamal <al.leonarduzzi@gmail.com>
2025-03-31 15:39:29 +0200

    Fix (probably) the excesive image size

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  // other config options can go here...
 };
 
 export default nextConfig;
